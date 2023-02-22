ac793b8f09ff576354ecb5f4b3f59f018d41cb2a
ndiritu-prisca <priscandiritu@outlook.com>
2023-02-22 14:51:57 +0000

    Include edge

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -68,6 +68,8 @@ void radix_sort(int *array, size_t size)
 	int max = get_max(array, size);
 	int exp;
 
+	if (array == NULL || size < 2)
+		return;
 	for (exp = 1; max / exp > 0; exp *= 10)
 		radix_count(array, size, exp);
 }
